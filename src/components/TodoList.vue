ede5e24ba93933917b7b97299b660779921d2c3b
suhail <suhail@onebitconsult.com>
2019-06-22 22:27:47 +0530

    added drag drop

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,20 +1,31 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>"
-    <input type="text" placeholder="whats need to be done?" class="input" v-model="newTodo" @keyup.enter="addTodo"/>
+    <h1>{{ msg }}</h1>
+    
+        <div  @dragover.prevent @drop.prevent @drop="dragFinish(-1, $event)" v-if="dragging > -1" class="trash-drop todo-item" >
+          Delete
+          </div>
+    
+    <div v-else>
+       <input type="text" placeholder="whats need to be done?" class="input" v-model="newTodo" @keyup.enter="addTodo"/>
+    </div>
+  
     <div class="row">
         <div class="inRow">
           <h4>Completed</h4>
-          <ul v-cloak v-for="(todo,index) in todos" :key="todo" class="todoItem">
+          <ul v-cloak v-for="(todo,index) in todos" :key="index" class="todoItem">
             <li v-if="todo.completed" @click="CompleteChange(index)">{{todo.item}}</li>
             <a v-if="todo.completed" @click="removeTodo(index)">Delete</a>
           </ul>
         </div>
         <div class="inRow">
           <h4>Pending</h4>
-          <ul v-cloak v-for="(todo,index) in todos" :key="todo" class="todoItem">
-            <li v-if="!todo.completed" @click="CompleteChange(index)">{{todo.item}}</li>
-            <a v-if="!todo.completed" @click="removeTodo(index)">Delete</a>
+          <ul v-cloak v-for="(todo,index) in todos" :key="index" class="todoItem">
+            <div draggable="true" @dragstart="dragStart(index, $event)" @dragover.prevent @dragenter="dragEnter" @dragleave="dragLeave" @dragend="dragEnd" @drop="dragFinish(index, $event)">
+              <li v-if="!todo.completed" @click="CompleteChange(index)">{{todo.item}}</li>
+              <a v-if="!todo.completed" @click="removeTodo(index)">Delete</a>
+            </div>
+
           </ul>
         </div>
   
@@ -24,6 +35,7 @@
 </template>
 
 <script>
+import { defaultCoreCipherList } from 'constants';
 export default {
   name: 'todo-list',
   props: {
@@ -33,6 +45,7 @@ export default {
       return{
        newTodo:'',
        todos:[],
+       dragging: -1
       }
   },
   methods:{
@@ -45,9 +58,54 @@ export default {
       },
       CompleteChange(ind){
           this.todos[ind].completed=!this.todos[ind].completed
+      },
+      dragStart(i,ev){
+        this.dragging = 1;
+        //  ev.dataTransfer.setData('Text', this.id);
+         ev.dataTransfer.dropEffect = 'move'
+         
+      },dragEnter(ev) {
+      /* 
+      if (ev.clientY > ev.target.height / 2) {
+        ev.target.style.marginBottom = '10px'
+      } else {
+        ev.target.style.marginTop = '10px'
       }
+      */
+    },removeItemAt(index) {
+      this.todos.splice(index, 1);
+    },
+    dragLeave(ev) {
+      
+      /* 
+      ev.target.style.marginTop = '2px'
+      ev.target.style.marginBottom = '2px'
+      */
+    },
+    dragEnd(ev) {
+      ev.preventDefault()
+      
+      this.dragging = -1;
+    },
+    dragFinish(to, ev) {
+      
+      this.moveItem(this.dragging, to);
+      ev.target.style.marginTop = '2px'
+      ev.target.style.marginBottom = '2px'
+    },
+    moveItem(from, to) {
+      
+      if (to === -1) {
+        this.removeTodo(from);
+        return this.dragging=1;
+      } else {
+        this.todos.splice(to, 0, this.todos.splice(from, 1)[0]);
+      }
+    }
+  },
+
   }
-}
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -91,5 +149,10 @@ a {
 }
 .todoItem{
 
+}
+.trash-drop {
+  border: 2px dashed #ccc !important;
+  text-align: center;
+  color: #e33;
 }
 </style>
